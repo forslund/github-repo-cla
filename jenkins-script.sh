f9c488d20815f3c5d776ce93701bd0968f5bf2b0
Åke Forslund <ake.forslund@gmail.com>
2019-06-11 17:46:06 +0200

    Add Script for CI

---
+++ jenkins-script.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+if [ ! -d .venv ]; then
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
+python cla_script.py --dry-run
