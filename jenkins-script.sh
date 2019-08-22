9bd64842f75d1fd34658fda10415dcf676c950b6
Åke Forslund <ake.forslund@gmail.com>
2019-08-22 16:38:14 +0200

    Always default to python3

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -3,8 +3,8 @@
 if [ ! -d .venv ]; then
     python3 -m venv .venv
     source .venv/bin/activate
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
 else
     source .venv/bin/activate
 fi
-python cla_script.py
+python3 cla_script.py
